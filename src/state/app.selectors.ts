a1546c1e7a67f9b776b35db802dfd893478774d0
eugenkin <evgensbk9@gmail.com>
2024-10-16 20:20:27 +0700

    create app selectors

---
+++ src/state/app.selectors.ts
@@ -0,0 +1,7 @@
+import { RootState } from "state/store"
+
+export const selectStatusApp = (state: RootState) => state.app.status
+export const selectAppIsLoggedIn = (state: RootState) => state.auth.isLoggedIn
+export const selectAppIsInitialized = (state: RootState) => state.app.isInitialized
+export const selectTasks = (state: RootState) => state.tasks
+export const selectTodolists = (state: RootState) => state.todolists
