fe8c890d99687f3c9a27de350739441e3aa5bc3a
eugenkin <evgensbk9@gmail.com>
2024-11-24 18:27:24 +0700

    create new selector

--- src/app/app.selectors.ts
+++ src/app/app.selectors.ts
@@ -5,3 +5,4 @@ export const selectAppIsLoggedIn = (state: RootState) => state.auth.isLoggedIn
 export const selectAppIsInitialized = (state: RootState) => state.app.isInitialized
 export const selectTasks = (state: RootState) => state.tasks
 export const selectTodolists = (state: RootState) => state.todolists
+export const selectAppError = (state: RootState) => state.app.error
