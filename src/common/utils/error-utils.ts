faeecf79b5b4b7aa27f69f4cf55d509f4a7569a5
eugenkin <evgensbk9@gmail.com>
2024-11-28 20:52:57 +0700

    added comments and conditions for declaring an error

--- src/common/utils/error-utils.ts
+++ src/common/utils/error-utils.ts
@@ -4,17 +4,27 @@ import { appActions } from "app/appSlice"
 import { AppThunkDispatch } from "app/store"
 import axios from "axios"
 
+/**
+ * Данная функция обрабатывает ошибки, которые могут возникнуть при взаимодействии с сервером.
+ * @param data  - ответ от сервера в формате ResponseType<D>
+ * @param dispatch - функция для отправки сообщений в store Redux
+ * @param showError - флаг, указывающий, нужно ли отображать ошибки в пользовательском интерфейсе
+ */
+
 // generic function
 export const handleServerAppError = <T>(
   data: ResponseType<T>,
   dispatch: Dispatch,
+  showError: boolean = true
 ) => {
-  if (data.messages.length) {
-    dispatch(appActions.setError(data.messages[0]))
-  } else {
-    dispatch(appActions.setError("Some error occurred"))
+  if(showError){
+    if (data.messages.length) {
+      dispatch(appActions.setError(data.messages[0]))
+    } else {
+      dispatch(appActions.setError("Some error occurred"))
+    }    
   }
-  dispatch(appActions.setStatus("failed"))
+  dispatch(appActions.setStatus("failed"))  
 }
 
 export const handleServerNetworkError = (
